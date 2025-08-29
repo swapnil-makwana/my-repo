a55fd6aefe8228e505e6eb0dff8fbe9243ea4b45
swapnil-makwana <swapnilmkwana@gmail.com>
2025-08-30 00:53:55 +0530

    Create positive_numbers.py

---
+++ positive_numbers.py
@@ -0,0 +1,7 @@
+list1 = [12, -7, 5, 64, -14]
+positive_numbers1 = [num for num in list1 if num > 0]
+print("Output:", positive_numbers1)
+
+list2 = [12, 14, -95, 3]
+positive_numbers2 = [num for num in list2 if num > 0]
+print("Output:", positive_numbers2)
